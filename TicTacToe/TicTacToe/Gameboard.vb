9ab430bbedf330242df5fa0ed97ae82b304add8a
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-23 14:21:48 +0800

    Modified Gameboard class so that the program knows if either x or o formed a three consecutive same marks(x or o) on a straight line, be it horizontal, vertical, diagonal, or a draw. If it does, the game will end and show a messagebox declaring the winner.

--- TicTacToe/TicTacToe/Gameboard.vb
+++ TicTacToe/TicTacToe/Gameboard.vb
@@ -28,5 +28,38 @@
         End If
 
         flag = Not flag
+        btn.Enabled = False
+        moveCount += 1
+
+        CheckStatus()
+    End Sub
+
+    Private Sub CheckStatus()
+        'check horizontals, verticals and diagonals for x
+        If matrix(0, 0) = "x" And matrix(0, 1) = "x" And matrix(0, 2) = "x" Or
+            matrix(1, 0) = "x" And matrix(1, 1) = "x" And matrix(1, 2) = "x" Or
+            matrix(2, 0) = "x" And matrix(2, 1) = "x" And matrix(2, 2) = "x" Or
+            matrix(0, 0) = "x" And matrix(1, 0) = "x" And matrix(2, 0) = "x" Or
+            matrix(0, 1) = "x" And matrix(1, 1) = "x" And matrix(2, 1) = "x" Or
+            matrix(0, 2) = "x" And matrix(1, 2) = "x" And matrix(2, 2) = "x" Or
+            matrix(0, 0) = "x" And matrix(1, 1) = "x" And matrix(2, 2) = "x" Or
+            matrix(0, 2) = "x" And matrix(1, 1) = "x" And matrix(2, 0) = "x" Then
+            MessageBox.Show("X wins!")
+            isGameOver = True
+            'check horizontals, verticals and diagonals for o
+        ElseIf matrix(0, 0) = "o" And matrix(0, 1) = "o" And matrix(0, 2) = "o" Or
+            matrix(1, 0) = "o" And matrix(1, 1) = "o" And matrix(1, 2) = "o" Or
+            matrix(2, 0) = "o" And matrix(2, 1) = "o" And matrix(2, 2) = "o" Or
+            matrix(0, 0) = "o" And matrix(1, 0) = "o" And matrix(2, 0) = "o" Or
+            matrix(0, 1) = "o" And matrix(1, 1) = "o" And matrix(2, 1) = "o" Or
+            matrix(0, 2) = "o" And matrix(1, 2) = "o" And matrix(2, 2) = "o" Or
+            matrix(0, 0) = "o" And matrix(1, 1) = "o" And matrix(2, 2) = "o" Or
+            matrix(0, 2) = "o" And matrix(1, 1) = "o" And matrix(2, 0) = "o" Then
+            MessageBox.Show("O wins!")
+            isGameOver = True
+        ElseIf moveCount = 9 Then
+            MessageBox.Show("Draw!")
+            isGameOver = True
+        End If
     End Sub
 End Class
--- TicTacToe/TicTacToe/UC-Gamescreen.vb
+++ TicTacToe/TicTacToe/UC-Gamescreen.vb
@@ -9,5 +9,55 @@
         gameboard = New Gameboard
     End Sub
 
+#Region "First Row"
+    Private Sub btn00_Click(sender As Object, e As EventArgs) Handles btn00.Click
+        Dim btn As Button = sender
+        gameboard.Move(0, 0, btn)
+    End Sub
+
+    Private Sub btn01_Click(sender As Object, e As EventArgs) Handles btn01.Click
+        Dim btn As Button = sender
+        gameboard.Move(0, 1, btn)
+    End Sub
+
+    Private Sub btn02_Click(sender As Object, e As EventArgs) Handles btn02.Click
+        Dim btn As Button = sender
+        gameboard.Move(0, 2, btn)
+    End Sub
+#End Region
+
+#Region "Second Row"
+    Private Sub btn10_Click(sender As Object, e As EventArgs) Handles btn10.Click
+        Dim btn As Button = sender
+        gameboard.Move(1, 0, btn)
+    End Sub
+
+    Private Sub btn11_Click(sender As Object, e As EventArgs) Handles btn11.Click
+        Dim btn As Button = sender
+        gameboard.Move(1, 1, btn)
+    End Sub
+
+    Private Sub btn12_Click(sender As Object, e As EventArgs) Handles btn12.Click
+        Dim btn As Button = sender
+        gameboard.Move(1, 2, btn)
+    End Sub
+#End Region
+
+#Region "Third Row"
+    Private Sub btn20_Click(sender As Object, e As EventArgs) Handles btn20.Click
+        Dim btn As Button = sender
+        gameboard.Move(2, 0, btn)
+    End Sub
+
+    Private Sub btn21_Click(sender As Object, e As EventArgs) Handles btn21.Click
+        Dim btn As Button = sender
+        gameboard.Move(2, 1, btn)
+    End Sub
+
+    Private Sub btn22_Click(sender As Object, e As EventArgs) Handles btn22.Click
+        Dim btn As Button = sender
+        gameboard.Move(2, 2, btn)
+    End Sub
+#End Region
 
 End Class
