e8cefbb78fb9621d8ec9ca985e67535004992964
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-07-02 05:50:01 +0800

    Modified 'Gameboard' class to accomodate the feature Player vs CPU game.

--- TicTacToe/TicTacToe/Gameboard.vb
+++ TicTacToe/TicTacToe/Gameboard.vb
@@ -5,67 +5,73 @@
 '   it also has a header
 '   and that header tells whose turn it is, and announces the winner
 Public Class Gameboard
-    Private ReadOnly matrix(3, 3) As Square
-    Private ReadOnly arbiter As Arbiter
-    Private isGameOver As Boolean
-    Private header As Label
+    Private _isGameOver As Boolean
+    Private _isXTurn As Boolean
+    Private _squareMatrix(3, 3) As Square
 
-    Public Sub New(ByVal headerFromFormmain As Label)
-        For i As Byte = 0 To matrix.GetLength(0) - 1
-            For j As Byte = 0 To matrix.GetLength(1) - 1
-                matrix(i, j) = New Square
+    Private _playerX As Player
+    Private _playerO As Player
+    Private _smartCPU As CPU
+    Private _isBattlingCPU As Boolean
+
+    Public Sub New(ByVal btnArray(,) As Button, ByVal isBattlingCPU As Boolean)
+        _isGameOver = False
+        _isXTurn = True
+        For i = 0 To _squareMatrix.GetLength(0) - 1
+            For j = 0 To _squareMatrix.GetLength(1) - 1
+                _squareMatrix(i, j) = New Square(btnArray(i, j))
             Next
         Next
 
-        arbiter = New Arbiter(matrix)
-
-        header = headerFromFormmain
-        header.ForeColor = Color.Yellow
-    End Sub
-
-    Public Sub Move(ByVal row As Byte, ByVal column As Byte, ByVal btn As Button)
-        If Not (isGameOver) Then
-            If arbiter.isXTurn Then
-                matrix(row, column).Trigger(True, btn)
-            Else
-                matrix(row, column).Trigger(False, btn)
-            End If
-            arbiter.moveCount += 1
-            isGameOver = arbiter.CheckIfWin()
-            HeaderUpdate()
-            arbiter.isXTurn = Not (arbiter.isXTurn)
-        End If
-    End Sub
-
-    Private Sub HeaderUpdate()
-        If Not (isGameOver) Then
-            If arbiter.isXTurn Then
-                header.Text = "Your turn O!"
-            ElseIf Not (arbiter.isXTurn) Then
-                header.Text = "Your turn X!"
-            End If
+        _playerX = New Player("x", _squareMatrix)
+        'check if 2P mode or CPU mode
+        If Not (isBattlingCPU) Then
+            _playerO = New Player("o", _squareMatrix)
         Else
-            If arbiter.isXTurn And Not (arbiter.moveCount = 9) Then
-                header.Text = "   X WINS!"
-                Blink()
-            ElseIf Not (arbiter.isXTurn) And Not (arbiter.moveCount = 9) Then
-                header.Text = "   O WINS!"
-                Blink()
-            Else
-                header.Text = "   DRAW!"
-                Blink()
-            End If
+            _smartCPU = New CPU("o", _squareMatrix)
+            _isBattlingCPU = isBattlingCPU
         End If
     End Sub
 
-    Private Async Sub Blink()
-        While True
-            Await Task.Delay(300)
-            If header.ForeColor = Color.Yellow Then
-                header.ForeColor = Color.Black
-            ElseIf header.ForeColor = Color.Black Then
-                header.ForeColor = Color.Yellow
+    Public Sub SquareUpdate(ByVal row As Byte, ByVal col As Byte)
+        If Not (_isGameOver) Then
+            If Not (_isBattlingCPU) Then
+                If _isXTurn Then
+                    _playerX.PutMark(_squareMatrix(row, col))
+                    _playerX.CheckIfWin(_isGameOver)
+                Else
+                    _playerO.PutMark(_squareMatrix(row, col))
+                    _playerO.CheckIfWin(_isGameOver)
+                End If
+
+                'check if draw
+                If _playerX.MoveCount + _playerO.MoveCount = 9 Then
+                    MessageBox.Show("Draw!")
+                    _isGameOver = True
+                End If
+
+                _isXTurn = Not (_isXTurn)
+            Else
+                _playerX.PutMark(_squareMatrix(row, col))
+                _playerX.CheckIfWin(_isGameOver)
+
+                'check if draw *needs refactoring
+                If _playerX.MoveCount + _smartCPU.MoveCount = 9 And Not (_isGameOver) Then
+                    MessageBox.Show("Draw!")
+                    _isGameOver = True
+                End If
+
+                If Not (_isGameOver) Then
+                    _smartCPU.PutMark()
+                    _smartCPU.CheckIfWin(_isGameOver)
+                End If
+
+                'check if draw *needs refactoring
+                If _playerX.MoveCount + _smartCPU.MoveCount = 9 And Not (_isGameOver) Then
+                    MessageBox.Show("Draw!")
+                    _isGameOver = True
+                End If
             End If
-        End While
+        End If
     End Sub
 End Class
